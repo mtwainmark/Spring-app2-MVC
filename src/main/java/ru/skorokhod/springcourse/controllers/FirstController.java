7359a71cd64546f706c536947889340f08ee98df
n.skorokhod <mtwain.mark@gmail.com>
2021-01-06 16:56:20 +0300

    Передача параметров в GET запрос и использование Model. Простой калькулятор.

--- src/main/java/ru/skorokhod/springcourse/controllers/FirstController.java
+++ src/main/java/ru/skorokhod/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.skorokhod.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +11,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/first")
 public class FirstController {
 
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") double b,
+                             @RequestParam(value = "action") String action,
+                             Model model){
+        double result;
+
+        switch (action){
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "+":
+                result = a + b;
+                break;
+            default: result = 0;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
+    }
+
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+        model.addAttribute("message", name + " " + surname);
         return "first/hello";
     }
 
